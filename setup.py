33cd40fa695d89c1234bf93951de9affdcd00db5
desbma <desbma@users.noreply.github.com>
2017-01-12 22:26:43 +0100

    Officially support Python 3.6

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(name="hddfancontrol",
                    "Programming Language :: Python :: 3.3",
                    "Programming Language :: Python :: 3.4",
                    "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6",
                    "Topic :: System :: Hardware",
                    "Topic :: System :: Monitoring",
                    "Topic :: Utilities"])
