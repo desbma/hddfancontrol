3d7a7081bd0d9160b30d07bf0a2dc9b0b7eec5f7
desbma <desbma@users.noreply.github.com>
2024-01-22 22:36:20 +0100

    feat: command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use clap::Parser;
+
+mod cl;
+
+fn main() -> anyhow::Result<()> {
+    // Parse cl args
+    let args = cl::Args::parse();
+
+    // Init logger
+    simple_logger::init_with_level(args.verbosity).context("Failed to init logger")?;
+
+    Ok(())
 }
