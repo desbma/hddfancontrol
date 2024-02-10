3e5c946c417abdc70377c5e2cb44acd155e28ffa
desbma <desbma@users.noreply.github.com>
2024-02-10 16:39:52 +0100

    feat: fan thresholds dynamic testing

--- src/fan.rs
+++ src/fan.rs
@@ -2,9 +2,23 @@
 
 #![allow(dead_code)]
 
-use std::{fmt, path::Path};
+use std::{cmp::Ordering, fmt, path::Path, thread::sleep, time::Duration};
 
-use crate::pwm::{ControlMode, Pwm};
+use crate::pwm::{self, ControlMode, Pwm};
+
+/// Fan characteristics
+pub struct Thresholds {
+    /// Minimum value at which the fan starts moving when it was stopped
+    min_start: pwm::Value,
+    /// Maximum value at which the fan stops moving when it was started
+    max_stop: pwm::Value,
+}
+
+impl fmt::Display for Thresholds {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(f, "{}/{}", self.min_start, self.max_stop)
+    }
+}
 
 /// Stateful fan
 pub struct Fan {
@@ -14,14 +28,34 @@ pub struct Fan {
     speed: Option<Speed>,
 }
 
+impl fmt::Display for Fan {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        self.pwm.fmt(f)
+    }
+}
+
 /// Fan speed as [0-255] value
 #[derive(Copy, Clone, PartialEq)]
 pub struct Speed(pub u8);
+
+impl Speed {
+    /// Maximum speed value
+    const MAX: Self = Self(u8::MAX);
+}
 
 impl fmt::Display for Speed {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         write!(f, "{:.2}%", f64::from(self.0) * 100.0 / f64::from(u8::MAX))
     }
+}
+
+/// Speed change direction
+#[derive(Copy, Clone)]
+enum SpeedChange {
+    /// Speed is increasing
+    Increasing,
+    /// Speed is decreasing
+    Decreasing,
 }
 
 impl Fan {
@@ -51,5 +85,83 @@ impl Fan {
             self.speed = Some(speed);
         }
         Ok(())
+    }
+
+    /// Wait until fan speed stop increasing or decreasing
+    fn wait_stable(&self, change: SpeedChange) -> anyhow::Result<()> {
+        /// Maximum duration to wait for the fan to be stabilized
+        const STABILIZE_TIMEOUT: Duration = Duration::from_secs(30);
+        /// Probe interval
+        const STABILIZE_PROBE_DELAY: Duration = Duration::from_millis(2000);
+
+        let mut time_waited = Duration::from_secs(0);
+        let mut prev_rpm = self.pwm.get_rpm()?;
+        debug_assert!((prev_rpm > 0) || matches!(change, SpeedChange::Increasing));
+        loop {
+            sleep(STABILIZE_PROBE_DELAY);
+            time_waited += STABILIZE_PROBE_DELAY;
+
+            let cur_rpm = self.pwm.get_rpm()?;
+            log::debug!("Fan {self} RPM: {cur_rpm}");
+
+            // We consider the fan speed stable if it changed less than 10% (if the value is significant),
+            // and if the direction changed
+            if (cur_rpm < 100) || (cur_rpm.abs_diff(prev_rpm) < (cur_rpm / 10)) {
+                #[allow(clippy::match_same_arms)]
+                match (cur_rpm.cmp(&prev_rpm), change) {
+                    (Ordering::Equal, _) => break,
+                    (Ordering::Greater, SpeedChange::Decreasing) => break,
+                    (Ordering::Less, SpeedChange::Increasing) => break,
+                    _ => (),
+                }
+            }
+
+            anyhow::ensure!(
+                time_waited < STABILIZE_TIMEOUT,
+                "Fan did not stabilize after {STABILIZE_TIMEOUT:?}"
+            );
+
+            prev_rpm = cur_rpm;
+        }
+        Ok(())
+    }
+
+    /// Is the fan physically moving?
+    fn is_moving(&self) -> anyhow::Result<bool> {
+        Ok(self.pwm.get_rpm()? > 0)
+    }
+
+    /// Dynamically test fan to find its thresholds
+    pub fn test(&mut self) -> anyhow::Result<Thresholds> {
+        self.set_speed(Speed::MAX)?;
+        self.wait_stable(SpeedChange::Increasing)?;
+        anyhow::ensure!(self.is_moving()?, "Fan is not moving at maximum speed");
+
+        let mut max_stop = 0;
+        for speed in (0..=u8::MAX).rev().step_by(5) {
+            self.set_speed(Speed(speed))?;
+            self.wait_stable(SpeedChange::Decreasing)?;
+            if !self.is_moving()? {
+                max_stop = speed;
+                break;
+            }
+        }
+        anyhow::ensure!(!self.is_moving()?, "Fan still moves at minimum speed");
+
+        let mut min_start = 0;
+        for speed in (0..=u8::MAX).step_by(5) {
+            self.set_speed(Speed(speed))?;
+            self.wait_stable(SpeedChange::Increasing)?;
+            if self.is_moving()? {
+                min_start = speed;
+                break;
+            }
+        }
+        anyhow::ensure!(self.is_moving()?, "Fan is not moving at maximum speed");
+
+        Ok(Thresholds {
+            min_start,
+            max_stop,
+        })
     }
 }
